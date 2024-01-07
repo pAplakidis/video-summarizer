863b0570ead8a1d808c373c3fc3eaaf3bd19f87f
pavlos_aplakidis <aplakidisp@gmail.com>
2024-01-07 18:31:55 +0200

    benchmark all videos

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-export VIDEO_IDX=$1
 export VERBOSE=0
 
-echo "Running clustering ..."
-cd clustering
-./extract_keyframes.py
+for ((i=0; i<25; i++))
+do
+  export VIDEO_IDX=$i
 
-cd ../
+  echo "Running clustering ..."
+  cd clustering
+  ./extract_keyframes.py
+  echo "\n"
+  cd ../
 
-echo "Running Neural Network ..."
-cd sum_gan_vae
-./inference.py
-
-cd ..
+  echo "Running Neural Network ..."
+  cd sum_gan_vae
+  ./inference.py
+  echo "\n"
+  cd ..
+done
 
